caa9125b4d5ad73009d9e499f9885bb20ec7041b
evgen <parom.evgen@mail.ru>
2023-05-05 22:03:03 +0300

    add two users inMemoryUserDetailsManager

--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
@@ -1,6 +1,7 @@
 package guru.sfg.brewery.web.controllers;
 
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.security.test.context.support.WithMockUser;
 
@@ -11,12 +12,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest
 public class BeerControllerIT extends BaseIT {
 
+    @Test
+    void initCreationForm() throws Exception{
+        mockMvc.perform(get("/beers/new").with(httpBasic("user", "password")))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/createBeer"))
+                .andExpect(model().attributeExists("beer"));
+    }
+
     @WithMockUser("spring")
     @Test
     void findBeers() throws Exception {
         mockMvc.perform(get("/beers/find"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("beers/findBeers"))
+                .andExpect(view().name("beers/findBeer"))
                 .andExpect(model().attributeExists("beer"));
     }
 
