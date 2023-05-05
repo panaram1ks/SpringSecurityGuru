e2a8d51f456675e09dc9cd4fb7f6f24d4a99cc86
evgen <parom.evgen@mail.ru>
2023-05-05 15:28:15 +0300

    test Get method use specific url

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package guru.sfg.brewery.config;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,6 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         authorize
                                 .antMatchers("/", "/webjars/**", "/login", "/resources/**").permitAll()
                                 .antMatchers("/beers/find","/beers*").permitAll()
+                                .antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll()
                 )
                 .authorizeRequests()
                 .anyRequest()
