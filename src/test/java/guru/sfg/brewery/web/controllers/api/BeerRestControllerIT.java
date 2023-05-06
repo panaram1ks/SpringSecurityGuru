f721f495c34200dac5d40c599bfcf4bdfa612187
evgen <parom.evgen@mail.ru>
2023-05-06 18:08:17 +0300

    custom AuthFilter csrf disable

--- src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
@@ -5,11 +5,22 @@ import guru.sfg.brewery.web.controllers.BaseIT;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
 public class BeerRestControllerIT extends BaseIT {
+
+    @Test
+    void deleteBeer() throws Exception {
+        mockMvc.perform(delete("/api/v1/beer/1111-1111111111-1111111111-1111")
+                        .header("Api-Key", "spring")
+                        .header("Api-Secret", "guru")
+                )
+                .andExpect(status().isOk());
+
+    }
 
     @Test
     void findBeers() throws Exception {
