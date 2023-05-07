1c70452de264f4be1996ed3eb3e947a2e7150a5e
evgen <parom.evgen@mail.ru>
2023-05-07 09:21:42 +0300

    use Lombock with entity

--- src/main/java/guru/sfg/brewery/domain/security/User.java
+++ src/main/java/guru/sfg/brewery/domain/security/User.java
@@ -1,8 +1,16 @@
 package guru.sfg.brewery.domain.security;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.Set;
 
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 public class User {
 
@@ -12,14 +20,23 @@ public class User {
     private String username;
     private String password;
 
+    @Singular
     @ManyToMany(cascade = CascadeType.MERGE)
     @JoinTable(name = "user_authority",
             joinColumns = {@JoinColumn(name = "USER_ID", referencedColumnName = "ID")},
             inverseJoinColumns = {@JoinColumn(name = "AUTHORITY_ID", referencedColumnName = "ID")})
     private Set<Authority> authorities;
+
+    @Builder.Default
     private Boolean accountNonExpired = true;
+
+    @Builder.Default
     private Boolean accountNonLocked = true;
+
+    @Builder.Default
     private Boolean credentialsNonExpired = true;
+
+    @Builder.Default
     private Boolean enable = true;
 
 }
